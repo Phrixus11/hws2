4bbd5cf9a3b5357a6008bef23ad3795ee4a29440
YuryR <myaccount136@gmail.com>
2025-04-17 17:48:06 +0300

    hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -14,14 +14,17 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         xType,
         className,
         disabled,
+
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+        + (disabled
+                ? ' ' + s.disabled
+                : xType === 'red'
+                    ? ' ' + s.red
+                : restProps.children === 'default'
+        ? ' ' + s.default : ' ' + s.secondary)
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
